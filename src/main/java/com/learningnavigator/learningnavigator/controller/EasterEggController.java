6688bbf361d78cad9c1888e456e659bd2e53b39f
Riyaz <sayyadriyaz0414@gmail.com>
2024-03-29 23:04:53 +0530

    Added eastern egg feature

---
+++ src/main/java/com/learningnavigator/learningnavigator/controller/EasterEggController.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class EasterEggController {
+
+    private static final String NUMBERS_API_URL = "http://numbersapi.com/{number}/trivia";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/hidden-feature/{number}")
+    public ResponseEntity<String> easterEgg(@PathVariable("number") int number) {
+        
+        String apiUrl = NUMBERS_API_URL.replace("{number}", String.valueOf(number));
+        String fact = restTemplate.getForObject(apiUrl, String.class);
+
+        if (fact != null) {
+            return ResponseEntity.ok(fact);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch fact from Numbers API");
+        }
+    }
+}
